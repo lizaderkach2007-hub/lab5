12f4ba0c26a9b366b1c4f3d5b257d555f0c73683
Liza <lizarderkach2007@knu.ua>
2026-03-06 02:17:42 +0200

    commit 04

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from utils import factorial, is_power_of_5
+
+print(factorial(5))
+print(is_power_of_5(125))
 from utils import factorial
 
 print(factorial(5))
--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+
+def is_power_of_5(n):
+    while n % 5 == 0 and n > 1:
+        n = n / 5
+    return n == 1
+
 def factorial(n):
     if n == 0:
         return 1
